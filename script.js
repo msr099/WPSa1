f08e4d95300e923298179fc008a52aac282852cd
msr099 <71330197+msr099@users.noreply.github.com>
2020-09-28 23:46:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function clickEvent1()
+{
+    window.open("studentExamRegistration.html", '_blank');
+}
+
+function clickEvent2()
+{
+    window.open("newQuiz.html", '_blank');
+}
+
+function clickEvent3()
+{
+    window.open("timetableEntry.html", '_blank');
+}
+
+function clickEvent4()
+{
+    window.open("studentRecord.html", '_blank');
+}
+
+function clickEvent5()
+{
+    window.open("removeStudent.html", '_blank'); 
+}
